6c67a3302618e11ec06ee7aa82138b22af2ba49f
eminaydin <eminaydin93@yahoo.com>
2020-05-30 18:00:50 +0200

    Details page

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,25 +1,41 @@
 import React, { useState, useEffect } from 'react';
-import { Input } from 'semantic-ui-react'
+import { Input, Button } from 'semantic-ui-react'
 import { connect } from 'react-redux';
 
 const ProductDetails = (props) => {
+    const [commentObject, setCommentObject] = useState({});
 
+    const clickHandler = () => {
+        props.addNewComment(commentObject)
 
-
+    }
 
     return (
         <div>
-            {props.post.text}
-            <Input />
+            {props.posts ? props.posts.text : null}
+
+            {props.comments.map(({ text }) => {
+                return <p>{text}</p>
+            })}
+            <Input value={commentObject.text}
+                onChange={comment => setCommentObject({ text: comment.target.value, date: new Date, id: Date.now() })} />
+            <Button onClick={clickHandler} />
         </div>
     );
 }
 const mapStateToProps = (state, ownProps) => {
-    console.log(ownProps);
+
     let slug = ownProps.match.params.slug;
 
     return {
-        post: state.posts.find(post => post.slug === slug)
+        ...state,
+        posts: state.posts.find(post => post.slug === slug),
+
     }
 }
-export default connect(mapStateToProps)(ProductDetails);
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addNewComment: (object) => { dispatch({ type: "ADD_COMMENT", payload: { comment: { text: object.text, date: object.date, id: object.id } } }) }
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(ProductDetails);
