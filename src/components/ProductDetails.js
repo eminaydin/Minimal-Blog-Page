86ec07a3ec3cdb148118ff6d29f10655434c0372
eminaydin <eminaydin93@yahoo.com>
2020-06-01 15:23:23 +0200

    Get the username

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,47 +1,76 @@
 import React, { useState, useEffect } from 'react';
-import { Input, Button } from 'semantic-ui-react'
+import { Input, Button, Comment, Header, Form } from 'semantic-ui-react'
 import { connect } from 'react-redux';
+import Moment from 'react-moment';
 
 const ProductDetails = (props) => {
 
 
     const [commentObject, setCommentObject] = useState({
+        userName: "",
         text: "",
         date: "",
         id: ""
     });
 
     const clickHandler = () => {
-        if (!commentObject.text.trim()) {
+        if (!commentObject.text.trim() || !commentObject.userName.trim()) {
             return
         }
         props.addNewComment(commentObject)
         setCommentObject({
             ...commentObject,
+            userName: "",
             text: ""
         })
-        console.log(commentObject.id);
+
     }
     useEffect(() => {
 
     }, []);
 
+
     return (
         <div>
             {props.posts ? props.posts.text : null}
-
+            <Header as='h3' dividing>
+                Comments
+                    </Header>
             {props.comments.filter(comment => {
                 return comment.postId === props.match.params.slug
-            }).map(({ text, id }) => {
+            }).map(({ text, id, date, userName }) => {
+                const dateToFormat = new Date(date)
 
-                return (<div key={id}>
-                    <p>{text}</p>
-                    <Button onClick={() => props.deleteComment(id)} >Delete comment</Button></div>)
+
+                return (
+                    <Comment.Group minimal key={id}>
+
+                        <Comment>
+                            <Comment.Avatar as='a' src='/images/avatar/small/matt.jpg' />
+                            <Comment.Content>
+                                <Comment.Author as='a'>{userName}</Comment.Author>
+                                <Comment.Metadata>
+                                    <span><Moment date={dateToFormat} fromNow ago /></span>
+                                </Comment.Metadata>
+                                <Comment.Text>{text}</Comment.Text>
+                                <Comment.Actions>
+                                    <a onClick={() => props.deleteComment(id)}>Delete</a>
+                                </Comment.Actions>
+                            </Comment.Content>
+                        </Comment>
+                    </Comment.Group>
+
+
+                )
             })}
-            <Input value={commentObject.text}
-                onChange={comment => setCommentObject({ text: comment.target.value, date: new Date(), id: Date.now(), postId: props.match.params.slug })}
-            />
-            <Button onClick={clickHandler} >Add comment</Button>
+            <Form reply>
+                <Input placeholder="Your username" className="input" required value={commentObject.userName} onChange={username => setCommentObject({ ...commentObject, userName: username.target.value })} />
+
+                <Form.TextArea value={commentObject.text} required
+                    onChange={comment => setCommentObject({ ...commentObject, text: comment.target.value, date: new Date(), id: Date.now(), postId: props.match.params.slug })} />
+                <Button content='Add Reply' labelPosition='left' icon='edit' primary onClick={clickHandler} />
+            </Form>
+
 
         </div>
     );
@@ -59,7 +88,7 @@ const mapStateToProps = (state, ownProps) => {
 }
 const mapDispatchToProps = (dispatch) => {
     return {
-        addNewComment: (object) => { dispatch({ type: "ADD_COMMENT", payload: { comment: { text: object.text, date: object.date, id: object.id, postId: object.postId } } }) },
+        addNewComment: (object) => { dispatch({ type: "ADD_COMMENT", payload: { comment: { userName: object.userName, text: object.text, date: object.date, id: object.id, postId: object.postId } } }) },
         deleteComment: (id) => { dispatch({ type: "DELETE_COMMENT", id: id }) }
     }
 }
