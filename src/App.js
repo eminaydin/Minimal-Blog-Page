88c6daf28258c02ad59007c8bff4b681778d549e
eminaydin <eminaydin93@yahoo.com>
2020-06-01 22:40:21 +0200

    Add navbar

--- src/App.js
+++ src/App.js
@@ -5,15 +5,20 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Link
 } from "react-router-dom";
 import ProductDetails from './components/ProductDetails';
 import Home from './components/Home';
 import data from "./data.json"
+import { Menu } from 'semantic-ui-react';
 
 function App() {
   return (
     <Router>
+      <Menu >
+        <Menu.Item icon="home" as={Link} to="/" />
 
+      </Menu>
       <Switch>
         <Route path={'/:slug'} render={(props) => <ProductDetails  {...props} />} />
         <Route path="/" render={(props) => <Home data={data}  {...props} />} />
