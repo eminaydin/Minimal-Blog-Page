f5681963c66da4a25d446e5ecfc2643d52808f6f
eminaydin <eminaydin93@yahoo.com>
2020-06-01 22:40:48 +0200

    Check comment quantity for correct rendering

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,24 +11,28 @@ const Home = (props) => {
             return comment.postId === slug
         });
     }
-
+    const checkComments = (slug) => {
+        if (filteredItems(slug).length === 0) return "No comments yet";
+        if (filteredItems(slug).length == 1) return filteredItems(slug).length + " comment";
+        if (filteredItems(slug).length > 1) return filteredItems(slug).length + " comments"
+    }
 
 
     return (
-        <Item.Group divided unstackable>
+        <Item.Group divided  >
 
 
 
             {data.map(({ slug, title, id, text }) => {
 
                 return <Item as={Link} to={`${slug}`} key={id}>
-                    <Item.Image size='small' src='/images/wireframe/image.png' />
+                    <Item.Image size='small' src='https://react.semantic-ui.com/images/wireframe/image.png' />
 
                     <Item.Content>
                         <Item.Header as='a'>{title}</Item.Header>
                         <Item.Description>{text}</Item.Description>
                         <Item.Extra>
-                            <Icon color='red' name='comment' /> {!filteredItems(slug).length <= 0 ? filteredItems(slug).length + " comments" : "No comment yet"}
+                            <Icon color='red' name='comment' /> {checkComments(slug)}
 
                         </Item.Extra>
                     </Item.Content>
