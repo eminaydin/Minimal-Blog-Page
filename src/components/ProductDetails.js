71b94379b4265bb74cddc570d7349e96a5407a68
eminaydin <eminaydin93@yahoo.com>
2020-06-01 13:25:40 +0200

    Delete comments dispatch, render comments on individual page

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -3,29 +3,53 @@ import { Input, Button } from 'semantic-ui-react'
 import { connect } from 'react-redux';
 
 const ProductDetails = (props) => {
-    const [commentObject, setCommentObject] = useState({});
+
+
+    const [commentObject, setCommentObject] = useState({
+        text: "",
+        date: "",
+        id: ""
+    });
 
     const clickHandler = () => {
+        if (!commentObject.text.trim()) {
+            return
+        }
         props.addNewComment(commentObject)
-
+        setCommentObject({
+            ...commentObject,
+            text: ""
+        })
+        console.log(commentObject.id);
     }
+    useEffect(() => {
+
+    }, []);
 
     return (
         <div>
             {props.posts ? props.posts.text : null}
 
-            {props.comments.map(({ text }) => {
-                return <p>{text}</p>
+            {props.comments.filter(comment => {
+                return comment.postId === props.match.params.slug
+            }).map(({ text, id }) => {
+
+                return (<div key={id}>
+                    <p>{text}</p>
+                    <Button onClick={() => props.deleteComment(id)} >Delete comment</Button></div>)
             })}
             <Input value={commentObject.text}
-                onChange={comment => setCommentObject({ text: comment.target.value, date: new Date, id: Date.now() })} />
-            <Button onClick={clickHandler} />
+                onChange={comment => setCommentObject({ text: comment.target.value, date: new Date(), id: Date.now(), postId: props.match.params.slug })}
+            />
+            <Button onClick={clickHandler} >Add comment</Button>
+
         </div>
     );
 }
 const mapStateToProps = (state, ownProps) => {
 
     let slug = ownProps.match.params.slug;
+
 
     return {
         ...state,
@@ -35,7 +59,8 @@ const mapStateToProps = (state, ownProps) => {
 }
 const mapDispatchToProps = (dispatch) => {
     return {
-        addNewComment: (object) => { dispatch({ type: "ADD_COMMENT", payload: { comment: { text: object.text, date: object.date, id: object.id } } }) }
+        addNewComment: (object) => { dispatch({ type: "ADD_COMMENT", payload: { comment: { text: object.text, date: object.date, id: object.id, postId: object.postId } } }) },
+        deleteComment: (id) => { dispatch({ type: "DELETE_COMMENT", id: id }) }
     }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(ProductDetails);
