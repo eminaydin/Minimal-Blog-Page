3c6e6822ca0d4885b0c696170374ed8147be99a1
eminaydin <eminaydin93@yahoo.com>
2020-06-02 12:34:24 +0200

    Add and delete comments within its own state

--- src/reducer/rootReducer.js
+++ src/reducer/rootReducer.js
@@ -1,23 +1,32 @@
 import data from ".././data.json"
 const initState = {
-    posts: data,
-    comments: []
+    posts: data
 }
 
 
 const rootReducer = (state = initState, action) => {
 
     if (action.type === "ADD_COMMENT") {
+        let findPost = state.posts.find(post => post.slug === action.payload.comment.postSlug);
+        let index = state.posts.indexOf(findPost);
+
         return {
             ...state,
-            comments: [...state.comments, action.payload.comment]
+            posts: [...state.posts,
+            state.posts[index].comments = [...state.posts[index].comments, action.payload.comment]]
         }
     }
     if (action.type === "DELETE_COMMENT") {
-        let newComments = state.comments.filter(comment => comment.id !== action.id)
+
+        let findPost = state.posts.find(post => post.slug === action.payload.slug);
+        let index = state.posts.indexOf(findPost);
+
+        let newComments = state.posts[index].comments.filter(comment => comment.id !== action.payload.id)
+
         return {
             ...state,
-            comments: newComments
+            posts: [...state.posts,
+            state.posts[index].comments = newComments]
         }
     }
 
